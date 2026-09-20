8e9c0ebe08249458df24d27afb5b102d8f5a702d
Kayla Alyssa <kaylaalyssa12345@gmail.com>
2026-09-20 13:01:55 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner jobs = new Scanner(Main.class.getResourceAsStream("jobs.txt"));
+
+        List<PrintJob> printList = new ArrayList<>();
+
+        while (jobs.hasNext()) {
+            String type = jobs.next();
+            String id = jobs.next();
+            int pages = jobs.nextInt();
+
+            if (type.equals("MONO")) {
+                printList.add(new MonoPrint(id, pages));
+            } else if (type.equals("COLOUR")) {
+                printList.add(new ColourPrint(id, pages));
+            }
+        }
+        jobs.close();
+
+        for (PrintJob job : printList) {
+            System.out.println(job.summary());
+        }
+    }
+}
